6d99bcc87eeeb422eea6fa2d65c645124dbe8068
aRookieCookie <arookiecookieyt@gmail.com>
2026-06-09 10:42:55 +0200

    finished variants model module

--- db_handler/models/variants.py
+++ db_handler/models/variants.py
@@ -11,3 +11,25 @@ def create(naam, max_health, fortitude_multi, mood_multi, thirst_multi, hunger_m
     conn.commit()
     conn.close()
     return varient_id
+
+def get_stats(docent_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM stats WHERE id = ?", (docent_id,))
+    row = cursor.fetchone()
+
+    return {
+        "naam" : row["naam"],
+        "max_health" : row["max_health"],
+        "fortitude" : row["fortitude_multi"],
+        "mood" : row["mood_multi"],
+        "thirst" : row["thirst_multi"],
+        "hunger" : row["hunger_multi"]
+    }
+
+def delete(docent_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM stats WHERE id = ?", (docent_id,))
+    conn.commit()
+    conn.close()
